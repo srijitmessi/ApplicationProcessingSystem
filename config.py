a7f399cf72eae233d6f9962cf8c56d99f5b03491
Riju Ghosh <ghoshriju33@gmail.com>
2017-09-06 15:00:37 +0530

    fixed raw string

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os.path
 
 db_path = os.path.abspath(os.path.dirname(__file__))
 db_uri = 'sqlite:///{}'.format(db_path)
-database_path = os.path.join(db_uri, 'db\users.db')
+database_path = os.path.join(db_uri, r'db\users.db')
