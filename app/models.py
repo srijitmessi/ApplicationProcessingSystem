0aa3976d73e764c7353c53c9b78fc2179efd0f47
mepsyduck <sumit231996@gmail.com>
2017-09-09 12:43:24 +0530

    updated model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from app import db
+import datetime
 from flask_login import UserMixin
+
 class User(db.Model,UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(120), unique=True)
@@ -15,6 +18,7 @@ class User(db.Model,UserMixin):
         return self.email
 
 class Student(db.Model):
+    __tablename__ = 'student'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
     vtuId = db.Column(db.String(8), unique=True)
@@ -29,6 +33,7 @@ class Student(db.Model):
         return self.email
 
 class Faculty(db.Model):
+    __tablename__ = 'faculty'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
     ttsId = db.Column(db.String(8), unique=True)
@@ -41,11 +46,30 @@ class Faculty(db.Model):
 
     def __repr__(self):
         return self.email
+
 class Post(db.Model):
+    __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(400))
     createdBy = db.Column(db.String(20))
     sentTo = db.Column(db.String(20))
     readStatus = db.Column(db.Boolean)
-    timeCreated = db.Column(db.DateTime, CheckConstraint())
+    timeCreated = db.Column(db.DateTime)
+    subject = db.Column(db.String(20))
 
+    def __init__(self, content, createdBy, sentTo,  subject ):
+        self.content = content
+        self.createdBy = createdBy
+	self.sentTo = sentTo 	
+	self.readStatus = False
+	self.timeCreated = datetime.datetime.now()
+	self.subject = subject
+
+    def markAsRead(self):
+	self.readStatus = True
+    
+    def markAsUnread(self):
+	self.readStatus = False
+
+    def __repr__(self):
+	return self.subject	
